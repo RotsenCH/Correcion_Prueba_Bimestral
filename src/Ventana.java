6448aa23925ef87c8105e59d350f6ce9e59f67d9
RotsenCH <117753370+RotsenCH@users.noreply.github.com>
2023-02-24 10:56:55 -0500

    Update Ventana.java

--- src/Ventana.java
+++ src/Ventana.java
@@ -11,27 +11,28 @@ public class Ventana extends JDialog {
     private JButton crearButton;
     private JButton actualizarButton;
     private JComboBox generoBOX;
-    private JComboBox edadBOX;
+    private JComboBox carreraBOX;
     private JTextField nombreTXT;
     private JTextField apellidoTXT;
     private JTextField cedulaTXT;
     private JTextField celularTXT;
 
     public Ventana() {
-
+        llenarComboBox_Género();
+        llenarComboBox_Carrera();
         crearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Connection con;
                 try {
                     con = getConection();
-                    ps = con.prepareStatement("INSERT INTO datos (nombre, apellido, cedula, genero, celular, edad) VALUES(?,?,?,?,?,?) ");
+                    ps = con.prepareStatement("INSERT INTO datos (nombre, apellido, cedula, genero, celular, carrera) VALUES(?,?,?,?,?,?) ");
                     ps.setString(1, nombreTXT.getText());
                     ps.setString(2, apellidoTXT.getText());
                     ps.setString(3, cedulaTXT.getText());
                     ps.setString(4, generoBOX.getSelectedItem().toString());
                     ps.setString(5, celularTXT.getText());
-                    ps.setString(6, edadBOX.getSelectedItem().toString());
+                    ps.setString(6, carreraBOX.getSelectedItem().toString());
                     System.out.println(ps);
                     int res = ps.executeUpdate();
 
@@ -71,7 +72,7 @@ public class Ventana extends JDialog {
                         nombreTXT.setText(rs.getString("nombre"));
                         apellidoTXT.setText(rs.getString("apellido"));
                         celularTXT.setText(rs.getString("celular"));
-                        edadBOX.setSelectedItem(rs.getString("edad"));
+                        carreraBOX.setSelectedItem(rs.getString("carrera"));
                         generoBOX.setSelectedItem(rs.getString("genero"));
                     } else {
                         JOptionPane.showMessageDialog(null, "No existe una persona con ese número de cédula");
@@ -87,18 +88,27 @@ public class Ventana extends JDialog {
         actualizarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                buscar_Estudiante();
                 Connection con;
 
                 try {
                     con = getConection();
-                    ps = con.prepareStatement("UPDATE datos SET nombre=?, apellido=?, genero=?, celular=");
+                    ps = con.prepareStatement("UPDATE datos SET nombre=?, apellido=?, genero=?, celular=?, carrera=? Where cedula = ?");
+
+                    ps.setString(1, nombreTXT.getText());
+                    ps.setString(2, apellidoTXT.getText());
+                    ps.setString(6, cedulaTXT.getText());
+                    ps.setString(3, generoBOX.getSelectedItem().toString());
+                    ps.setString(4, celularTXT.getText());
+                    ps.setString(5, carreraBOX.getSelectedItem().toString());
+                    System.out.println(ps);
 
                     int res = ps.executeUpdate();
 
                     if (res > 0) {
-                        JOptionPane.showMessageDialog(null, "Persona Modificada");
+                        JOptionPane.showMessageDialog(null, "Estudiante Modificado");
                     } else {
-                        JOptionPane.showMessageDialog(null, "Error al Modificar persona");
+                        JOptionPane.showMessageDialog(null, "Error al Modificar Estudiante, ingrese un estudiante válido");
                     }
 
                     limpiar();
@@ -113,11 +123,57 @@ public class Ventana extends JDialog {
         borrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                buscar_Estudiante();
                 Connection con;
 
-                limpiar();
+                try {
+                    con = getConection();
+                    ps = con.prepareStatement("DELETE FROM datos  Where cedula = ?");
+                    ps.setString(1, cedulaTXT.getText());
+
+                    System.out.println(ps);
+
+                    int res = ps.executeUpdate();
+
+                    if (res > 0) {
+                        JOptionPane.showMessageDialog(null, "Estudiante Eliminado Satisfactoriamente");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Error al Eliminar Estudiante, ingrese un estudiante válido");
+                    }
+
+                    limpiar();
+                    con.close();
+
+                } catch (HeadlessException | SQLException f) {
+                    System.err.println(f);
+                }
             }
         });
+    }
+
+    private void buscar_Estudiante(){
+        Connection con;
+
+        try {
+            con = getConection();
+            String query = "select * from datos;";
+            Statement s = con.createStatement();
+            ps = con.prepareStatement("SELECT * FROM datos WHERE cedula = ?");
+            ps.setString(1, cedulaTXT.getText());
+            ResultSet rs = s.executeQuery(query);
+
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Estudiante Encontrado");
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe una persona con ese número de cédula");
+                limpiar();
+            }
+
+        } catch (HeadlessException | SQLException f) {
+            System.err.println(f);
+        }
     }
 
     private void limpiar() {
@@ -126,7 +182,43 @@ public class Ventana extends JDialog {
         generoBOX.setSelectedIndex(0);
         cedulaTXT.setText(null);
         celularTXT.setText(null);
-        edadBOX.setSelectedIndex(0);
+        carreraBOX.setSelectedIndex(0);
+    }
+
+    private void llenarComboBox_Género(){
+        Connection con;
+        try {
+            con = getConection();
+            String query = "SELECT * FROM Generos_ComboBox";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()){
+                generoBOX.addItem(rs.getString(1));
+            }
+            con.close();
+
+        } catch (HeadlessException | SQLException f) {
+            System.err.println(f);
+        }
+    }
+
+    private void llenarComboBox_Carrera(){
+        Connection con;
+        try {
+            con = getConection();
+            String query = "SELECT * FROM Carrera_ComboBox";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()){
+                carreraBOX.addItem(rs.getString(1));
+            }
+            con.close();
+
+        } catch (HeadlessException | SQLException f) {
+            System.err.println(f);
+        }
     }
 
     public static Connection getConection(){
@@ -142,6 +234,7 @@ public class Ventana extends JDialog {
         }
         return con;
     }
+
 
     public static void main(String[] args) {
         JFrame frame =new JFrame("Néstor Chumania");
